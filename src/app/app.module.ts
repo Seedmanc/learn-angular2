6ea3f94de84b3ec24a40fda8fd5a1c560e9eb539
Seedmanc <seedmanc@gmail.com>
2017-11-21 14:36:55 +0300

    #31 + practice 2
    two-way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from "./warning-alert/warning-alert.component";
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { Practice1Component } from './practice1/practice1.component';
+import { Practice2Component } from './practice2/practice2.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    Practice1Component,
+    Practice2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
