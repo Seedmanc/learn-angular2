54be88d2da5c203fb35aa8abe5376a8bf43b65c8
Seedmanc <seedmanc@gmail.com>
2017-11-22 12:56:24 +0300

    #43 navbar

--- recipes/src/app/app.module.ts
+++ recipes/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-list/shopping-edit/shopping-edit.component';
 import { RecipesComponent } from './recipes/recipes.component';
 
 
@@ -21,7 +21,7 @@ import { RecipesComponent } from './recipes/recipes.component';
     RecipeItemComponent,
     RecipeDetailComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
