33b37673cdd2c8df9631f60fca2c53807d495a31
Seedmanc <seedmanc@gmail.com>
2017-11-22 18:07:47 +0300

    #75 & practice 4

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 **https://seedmanc.github.io/learn-angular2/my-first-app** <= working practice lessons
+
 **https://seedmanc.github.io/learn-angular2/recipes** <= working course project
--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from "./warning-alert/warning-alert.component";
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from "./practice1/warning-alert/warning-alert.component";
+import { SuccessAlertComponent } from './practice1/success-alert/success-alert.component';
 import { Practice1Component } from './practice1/practice1.component';
 import { Practice2Component } from './practice2/practice2.component';
 import {FormsModule} from "@angular/forms";
 import {Practice3Component} from "./practice3/practice3.component";
+import { Practice4Component } from './practice4/practice4.component';
+import { GameControlComponent } from './practice4/game-control/game-control.component';
+import { OddComponent } from './practice4/odd/odd.component';
+import { EvenComponent } from './practice4/even/even.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,11 @@ import {Practice3Component} from "./practice3/practice3.component";
     SuccessAlertComponent,
     Practice1Component,
     Practice2Component,
-    Practice3Component
+    Practice3Component,
+    Practice4Component,
+    GameControlComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule,
