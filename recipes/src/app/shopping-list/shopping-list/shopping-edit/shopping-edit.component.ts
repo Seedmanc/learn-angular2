7c9dcc9dfd9a711807c9ad166362757e0c51b4ad
Seedmanc <seedmanc@gmail.com>
2017-11-23 12:30:31 +0300

    #80 shopping list adding

--- recipes/src/app/shopping-list/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipes/src/app/shopping-list/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter, ElementRef } from '@angular/core';
+import {Ingredient} from "../../../shared/ingredient.model";
 
 @Component({
   selector: 'shopping-edit',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @Output() ingAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  addItem(amount: number) {
+    this.ingAdded.emit(new Ingredient(this.nameInput.nativeElement.value, amount));
+  }
 }
--- recipes/src/app/shopping-list/shopping-list/shopping-list.component.ts
+++ recipes/src/app/shopping-list/shopping-list/shopping-list.component.ts
@@ -18,4 +18,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  addIng(ing: Ingredient){
+    this.ingredients.push(ing);
+  }
+
 }
