cb78cbd122b73e2060cbf09635bca16d195c58fd
Seedmanc <seedmanc@gmail.com>
2017-11-24 15:20:53 +0300

    #110 adding ingredients from recipes

--- recipes/src/app/recipes/recipe.model.ts
+++ recipes/src/app/recipes/recipe.model.ts
@@ -1,9 +1,11 @@
+import {Ingredient} from "../shared/ingredient.model";
 export class Recipe {
   public name: string;
   public descr: string;
   public img: string;
+  public ingredients: Ingredient[];
 
-  constructor(name: string, descr: string, imgpath: string) {
-    Object.assign(this, {name, descr, imgpath});
+  constructor(name: string, descr: string, imgpath: string, ingredients: Ingredient[]) {
+    Object.assign(this, {name, descr, imgpath, ingredients});
   }
 }
--- recipes/src/app/recipes/recipes.service.ts
+++ recipes/src/app/recipes/recipes.service.ts
@@ -1,19 +1,34 @@
 import { Injectable, Output, EventEmitter } from '@angular/core';
 import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
 @Injectable()
 export class RecipesService {
   recipeSelected = new EventEmitter<Recipe>();
 
+  constructor(private shopService: ShoppingListService){}
+
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a test', 'https://d30y9cdsu7xlg0.cloudfront.net/png/82540-200.png'),
-    new Recipe('Another test recipe', 'this is again a test', 'https://d30y9cdsu7xlg0.cloudfront.net/png/82545-200.png'),
+    new Recipe('Schnitzel', 'this is a test', 'https://d30y9cdsu7xlg0.cloudfront.net/png/82540-200.png',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ),
+    new Recipe('Burger', 'this is again a test', 'https://d30y9cdsu7xlg0.cloudfront.net/png/82545-200.png',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]),
   ];
 
   getRecipes() {
     return this.recipes.slice();
   }
 
-  constructor() { }
+  addToShop(ingredients: Ingredient[]) {
+    this.shopService.addIngs(ingredients);
+  }
 
 }
