89c23a463d12fc24c21f8f19fb6cbf3e5e7a992c
Seedmanc <seedmanc@gmail.com>
2017-12-02 23:17:34 +0300

    #365 keyframes groups

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {animate, Component, state, style, transition, trigger} from '@angular/core';
+import {animate, Component, state, style, transition, trigger, group,keyframes} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -53,6 +53,47 @@ import {animate, Component, state, style, transition, trigger} from '@angular/co
         animate(400)
       ]),
       transition('* => void', [
+        group([
+          animate(300, style({
+            color: 'red'
+          })),
+          animate(600, style({
+            transform: 'translateX(100px)',
+            opacity: 0
+          }))
+        ])
+      ])
+    ]),
+    trigger('list2', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            'opacity': 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            'opacity': 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            'opacity': 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0)',
+            'opacity': 1,
+            offset: 1
+          }),
+        ]))
+      ]),
+      transition('* => void', [
         animate(400, style({
           transform: 'translateX(100px)',
           opacity: 0
@@ -78,6 +119,12 @@ export class AppComponent {
   }
   onShrink() {
     this.wild = 'shrunken';
+  }
+  animStart(e){
+    console.log(e)
+  }
+  animEnd(e){
+    console.log(e)
   }
 
 }
